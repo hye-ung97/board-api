c1702b4e0780916f80479c5bfa28d336b59a956e
혜영 <hysung714@naver.com>
2025-09-07 06:45:37 +0000

    feat: add h2

--- src/test/java/org/board/board/BoardApplicationTests.java
+++ src/test/java/org/board/board/BoardApplicationTests.java
@@ -2,8 +2,10 @@ package org.board.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BoardApplicationTests {
 
   @Test
