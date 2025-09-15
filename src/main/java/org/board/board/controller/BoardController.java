a39a84e3260306e4fcbedab9d61aef2b1a1f8e73
혜영 <hysung714@naver.com>
2025-09-15 06:15:16 +0000

    feat: board list and get by id

--- src/main/java/org/board/board/controller/BoardController.java
+++ src/main/java/org/board/board/controller/BoardController.java
@@ -4,7 +4,11 @@ import jakarta.validation.Valid;
 
 import org.board.board.dto.board.BoardCreateRequest;
 import org.board.board.dto.board.BoardCreateResponse;
+import org.board.board.dto.board.BoardListResponse;
+import org.board.board.entity.Board;
 import org.board.board.service.BoardService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -52,6 +56,57 @@ public class BoardController {
       BoardCreateResponse response = boardService.createBoard(request, memberId);
 
       return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+  }
+
+  @Operation(summary = "게시글 리스트", description = "게시글 목록을 불러옵니다.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "게시글 목록 불러오기 성공",
+            content = @Content(schema = @Schema(implementation = BoardListResponse.class))),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
+        @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자"),
+        @ApiResponse(responseCode = "500", description = "서버 오류")
+      })
+  @GetMapping
+  public ResponseEntity<Page<BoardListResponse>> getBoardList(final Pageable pageable) {
+    try {
+      Page<BoardListResponse> response = boardService.getBoardsList(pageable);
+
+      return ResponseEntity.status(HttpStatus.OK).body(response);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+  }
+
+  @Operation(summary = "게시글 상세", description = "특정 게시글의 상세 정보를 불러옵니다.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "게시글 상세 불러오기 성공",
+            content = @Content(schema = @Schema(implementation = Board.class))),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
+        @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자"),
+        @ApiResponse(responseCode = "404", description = "게시글을 찾을 수 없음"),
+        @ApiResponse(responseCode = "500", description = "서버 오류")
+      })
+  @GetMapping("/{id}")
+  public ResponseEntity<Board> getBoardById(@PathVariable Long id) {
+    try {
+      Board response = boardService.getBoardById(id);
+      if (response == null) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+      }
+      return ResponseEntity.status(HttpStatus.OK).body(response);
     } catch (IllegalArgumentException e) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     } catch (Exception e) {
